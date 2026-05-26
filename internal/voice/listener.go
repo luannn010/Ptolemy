317f9c2021600bf854dbb1ebd1c6b032a82444e5
luannn010 <luannn010@gmail.com>
2026-05-26 22:51:25 +1000

    chore(main): remove voice subsystem from v2 baseline

--- internal/voice/listener.go
+++
@@ -1,7 +0,0 @@
-package voice
-
-import "context"
-
-type Listener interface {
-	Listen(ctx context.Context) (<-chan string, error)
-}
--- third_party/vosk-go/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/alphacep/vosk-api/go
-
-go 1.16
